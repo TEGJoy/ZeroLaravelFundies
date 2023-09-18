8f0b9e91588fec3e05e40c8fd38ef9fde13b24b3
TEGJoy <terencio_12@live.nl>
2023-09-18 15:03:22 +0200

    Migration setup
    They exist now

--- database/migrations/2023_09_18_123057_create_tournaments_table.php
+++ database/migrations/2023_09_18_123057_create_tournaments_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->bigInteger('max');
+            $table->smallInteger('is_active');
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('tournaments');
     }
 };
